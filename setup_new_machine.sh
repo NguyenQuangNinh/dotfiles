c470024a70d7141e4a9f3de5ac5e3678d17ad93f
Ninh Nguyen <quangninh@gmail.com>
2016-02-24 19:44:12 -0800

    Setup new machine

--- setup_new_machine.sh
+++ setup_new_machine.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
 # Add Sublime Text PPA (-y for unattended process)
-#sudo apt-add-repository -y ppa:webupd8team/sublime-text-3
-# Add NodeJS PPA
-#sudo apt-add-repository -y ppa:chris-lea/node.js
-wget -qO- https://deb.nodesource.com/setup_4.x | sudo bash -
-# Add Ubuntu Tweak PPA
-#sudo apt-add-repository -y ppa:tualatrix/ppa
-#sudo apt-get install ubuntu-tweak
-#
+sudo apt-add-repository -y ppa:webupd8team/sublime-text-3
+
+#Google Chrome
 sudo apt-add-repository -y 'deb http://dl.google.com/linux/chrome/deb/ stable main'
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 
@@ -18,20 +13,18 @@ sudo apt-get update
 # Install neccessary packages:
 sudo apt-get install git subversion meld ubuntu-restricted-extras
 
-# Install Google Chrome Canary
-sudo apt-get install google-chrome-unstable
+# Install Google Chrome
+sudo apt-get install google-chrome-stable
 
 # Install Sublime Text
-#sudo apt-get install sublime-text-installer
+sudo apt-get install sublime-text-installer
+
+cd ~/Desktop
+git clone https://github.com/NguyenQuangNinh/dotfiles
+cd dotfiles
 
 # Sync my Sublime Text 3 settings
-rsync --exclude ".git/" --exclude ".DS_Store" --exclude "README.md" -av ../sublimetext/Packages ~/.config/sublime-text-3
-
-# Package Control for ST3: (ST3 will automatically install packages via previous settings)
-# cd ~/.config/sublime-text-3/Packages/
-# git clone https://github.com/wbond/sublime_package_control.git "Package Control"
-# cd "Package Control"
-# git checkout python3
+rsync --exclude ".git/" --exclude ".DS_Store" --exclude "README.md" -av ./sublimetext/Packages ~/.config/sublime-text-3
 
 # Sublime Linter for ST3
 # TODO
@@ -40,9 +33,11 @@ rsync --exclude ".git/" --exclude ".DS_Store" --exclude "README.md" -av ../subli
 # TODO
 
 # Install NodeJS
+wget -qO- https://deb.nodesource.com/setup_4.x | sudo bash -
 sudo apt-get install python-software-properties python g++ make
 sudo apt-get install --yes nodejs
-npm install -g grunt-cli jshint jscs csslint yo bower http-server marked
+npm install -g grunt-cli jshint jscs csslint yo bower http-server marked eslint
+eslint --init
 
 # Install Nginx
 sudo apt-get install nginx
