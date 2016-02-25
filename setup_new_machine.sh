f7e73f33e96ac14a9aef965d7c913d640f655f81
Ninh Nguyen <quangninh@gmail.com>
2016-02-25 00:41:42 -0800

    Change ownership of sublime config to current user
    Turn off auto lock and turn off monitor screen

--- setup_new_machine.sh
+++ setup_new_machine.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Don't turn off my screen
+gsettings set org.gnome.settings-daemon.plugins.power sleep-display-ac 0
+gsettings set org.gnome.settings-daemon.plugins.power sleep-display-battery 0
+gsettings set org.gnome.desktop.session idle-delay 0
+
 # Add Sublime Text PPA (-y for unattended process)
 sudo apt-add-repository -y ppa:webupd8team/sublime-text-3
 
@@ -8,16 +13,16 @@ sudo apt-add-repository -y 'deb http://dl.google.com/linux/chrome/deb/ stable ma
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 
 # Update all repo
-sudo apt-get update
+sudo apt-get -y update
 
 # Install neccessary packages:
-sudo apt-get install git subversion meld ubuntu-restricted-extras
+sudo apt-get -y install git subversion meld ubuntu-restricted-extras
 
 # Install Google Chrome
-sudo apt-get install google-chrome-stable
+sudo apt-get -y install google-chrome-stable
 
 # Install Sublime Text
-sudo apt-get install sublime-text-installer
+sudo apt-get -y install sublime-text-installer
 
 cd ~/Desktop
 git clone https://github.com/NguyenQuangNinh/dotfiles
@@ -25,6 +30,7 @@ cd dotfiles
 
 # Sync my Sublime Text 3 settings
 rsync --exclude ".git/" --exclude ".DS_Store" --exclude "README.md" -av ./sublimetext/Packages ~/.config/sublime-text-3
+sudo chown -R $USER: ~/.config/sublime-text-3/
 
 # Sublime Linter for ST3
 # TODO
@@ -34,21 +40,21 @@ rsync --exclude ".git/" --exclude ".DS_Store" --exclude "README.md" -av ./sublim
 
 # Install NodeJS
 wget -qO- https://deb.nodesource.com/setup_4.x | sudo bash -
-sudo apt-get install python-software-properties python g++ make
-sudo apt-get install --yes nodejs
+sudo apt-get -y install python-software-properties python g++ make
+sudo apt-get -y install --yes nodejs
 # Update npm to latest version
 sudo npm install -g npm
 sudo npm install -g grunt-cli jshint jscs csslint yo bower http-server marked eslint
 eslint --init
 
 # Install Nginx
-sudo apt-get install nginx
+sudo apt-get -y install nginx
 
 # Install Ubuntu Tweak
-sudo apt-get install unity-tweak-tool
+sudo apt-get -y install unity-tweak-tool
 
 # Install ibus-unikey
-sudo apt-get install ibus-unikey
+sudo apt-get -y install ibus-unikey
 # Mannual: Add ibus-daemon to start up program
 
 # TODO: install Source Code Pro fonts via git & command line
