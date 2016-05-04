a0cb22e0aac5c8df5d3779bfc3637ec0f7eedf7f
Ninh Nguyen <quangninh@gmail.com>
2016-05-04 15:57:29 +0700

    install ruby url

--- installer.sh
+++ installer.sh
@@ -49,7 +49,7 @@ eslint --init
 
 # Install Ruby & RVM
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-curl -L get.rvm.io | bash -s stable
+curl -sSL https://get.rvm.io | bash -s stable
 
 if [ -f /etc/profile.d/rvm.sh ];
 then
